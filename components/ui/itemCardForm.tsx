2ebfc36a5a68cca407740f63cd71409e286962f0
Steven Le <stevenleusa79@yahoo.com>
2024-04-28 09:55:34 -0700

    styling again

--- components/ui/itemCardForm.tsx
+++ components/ui/itemCardForm.tsx
@@ -4,16 +4,18 @@ import { Progress } from "./progress";
 const ItemCardForm = () => {
   return (
     <div className="card w-80 bg-gray-500 mb-4">
-      <div className="h-[275px] bg-gray-500 w-full">
+      <div className="h-[275px] bg-gray-500 p-5 rounded-lg text-center">
+        <p className="">Add New Item</p>
+        <div className="divider my-1 before:bg-black after:bg-black"/>
         <input
           defaultValue="image url"
-          className="overflow-hidden w-full text-lg"
-        ></input>
+          className="overflow-hidden w-full text-lg rounded-md p-1"
+        />
       </div>
       <div className="card-body text-sm">
-        <input className="card-title" defaultValue="Name: " />
-        <textarea defaultValue="Description: "></textarea>
-        <div>
+        <input className="card-title rounded-md p-1" defaultValue="Name: " />
+        <textarea className="rounded-md p-1" defaultValue="Description: "></textarea>
+        <div className="hidden">
           <Progress value={50} />
           <p>
             {50}/{100}
