22fd64b668824febf895574ead0f92c9f605413c
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-04-28 13:16:56 -0700

    update email to include an unsubscribe link

--- actions/addToMailingList.ts
+++ actions/addToMailingList.ts
@@ -3,18 +3,17 @@ import prisma from "@/lib/prisma";
 import { Email } from "@prisma/client";
 
 export default async function addToMailingList(email: string) {
-  // Update db
-  prisma.email.create({ data: { email: email } });
-
-  // send initial thank you letter
   const resend = new Resend(process.env.RESEND_API);
-  resend.emails.send({
-    from: "DCMH@pantrypatrol.tech",
-    to: email,
-    subject: "Thanks for joining us!",
-    html: "<p>Thanks for joining our mailing list! We'll keep you updated on all the latest news and events.<br>We appreciate all our donors and are honored that you want to know when we need more donations.</p>",
-    headers: {
-      "List-Unsubscribe": "<https://pantrypatrol.tech/unsubscribe>",
-    },
-  });
+  await Promise.all([
+    prisma.email.create({ data: { email: email } }),
+    resend.emails.send({
+      from: "DCMH@pantrypatrol.tech",
+      to: email,
+      subject: "Thanks for joining us!",
+      html: `<p>Thanks for joining our mailing list! We'll keep you updated on all the latest news and events.<br>We appreciate all our donors and are honored that you want to know when we need more donations.<br>Feel free to <a href='https://pantrypatrol.tech/unsubscribe?email="${email}"'>unsubscribe</a> at any time </p>`,
+      headers: {
+        "List-Unsubscribe": "<https://pantrypatrol.tech/unsubscribe>",
+      },
+    }),
+  ]);
 }
