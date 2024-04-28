540cecfb78435c3f2f23910307252ebf8b81481c
Steven Le <stevenleusa79@yahoo.com>
2024-04-28 02:39:42 -0700

    images from all domains lol

--- next.config.mjs
+++ next.config.mjs
@@ -4,36 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "img.propelauth.com",
-      },
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-      },
-      {
-        protocol: "https",
-        hostname: "media.istockphoto.com",
-      },
-      {
-        protocol: "https",
-        hostname:
-          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt_I-fsOalFcGTCJYuAZQTo6tSFYbzMZmG-v-ViOymwA&s",
-      },
-      {
-        protocol: "https",
-        hostname: "m.media-amazon.com",
-      },
-      {
-        protocol: "https",
-        hostname: "encrypted-tbn0.gstatic.com",
-      },
-      {
-        protocol: "https",
-        hostname: "i5.walmartimages.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.google.com"
+        hostname: "*",
       }
     ],
   },
