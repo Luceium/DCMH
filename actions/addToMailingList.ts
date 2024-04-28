25f7212c60097d8fa04e7d45d5b4a21c0f6a550a
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-04-28 13:01:14 -0700

    make email sender

--- actions/addToMailingList.ts
+++ actions/addToMailingList.ts
@@ -1,3 +1,4 @@
+import { Resend } from "resend";
 import prisma from "@/lib/prisma";
 import { Email } from "@prisma/client";
 
@@ -6,4 +7,14 @@ export default async function addToMailingList(email: string) {
   prisma.email.create({ data: { email: email } });
 
   // send initial thank you letter
+  const resend = new Resend(process.env.RESEND_API);
+  resend.emails.send({
+    from: "DCMH@pantrypatrol.tech",
+    to: email,
+    subject: "Thanks for joining us!",
+    html: "<p>Thanks for joining our mailing list! We'll keep you updated on all the latest news and events.<br>We appreciate all our donors and are honored that you want to know when we need more donations.</p>",
+    headers: {
+      "List-Unsubscribe": "<https://pantrypatrol.tech/unsubscribe>",
+    },
+  });
 }
