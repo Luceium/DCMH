1ace20352470e4feaf212ded87b773d106d0951f
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-04-27 20:23:51 -0700

    update config and add

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "img.propelauth.com",
       },
+      {
+        protocol: "https",
+        hostname: "media.istockphoto.com",
+      }
     ],
   },
 };
