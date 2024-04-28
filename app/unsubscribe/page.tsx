682e4a51143ce082aba433ecba5af7f0ed2dfc3d
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-04-28 13:25:53 -0700

    allow unsubscribe

---
+++ app/unsubscribe/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const UnsubscribePage = () => {
+  return (
+    <h1>
+      We're sorry to see you go.
+      <br />
+      <br />
+      You have been unsubscribed from our mailing list.
+      <br />
+      Please feel free to continue donating.
+    </h1>
+  );
+};
+
+export default UnsubscribePage;
---
+++ app/unsubscribe/route.ts
@@ -0,0 +1,18 @@
+import prisma from "@/lib/prisma";
+import { redirect } from "next/navigation";
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  // get email
+  const email = searchParams.get("email");
+  if (!email) {
+    throw new Error("No email provided");
+  }
+  // delete email from db
+  await prisma.email.deleteMany({
+    where: { email: email },
+  });
+
+  redirect("/unsubscribe");
+}
