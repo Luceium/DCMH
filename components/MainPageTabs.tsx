8631140568fb6370d3a9912bc34262a47be25413
NotMyPersonalAccount <kevinzj710@gmail.com>
2024-07-29 18:56:42 -0700

    Fix build

--- components/MainPageTabs.tsx
+++ components/MainPageTabs.tsx
@@ -3,12 +3,12 @@
 import { Tabs } from "./ui/tabs";
 import { Item } from "@prisma/client";
 import ItemCard from "./ItemCard";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 import { produce } from "immer";
 import { fetchItems } from "@/actions/fetchItems";
 import { EditContext } from "@/lib/context";
 import ItemCardForm from "./ui/itemCardForm";
-import { useRouter, useSearchParams } from "next/navigation";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 type Tab = {
   title: string;
@@ -18,123 +18,125 @@ type Tab = {
 
 export default function MainPageTabs({ items: _items }: { items: Item[] }) {
   const [items, setItems] = useState(_items);
-  const searchParams = useSearchParams();
   const router = useRouter();
-  const [activeTabName, setActiveTabName] = useState(
-    searchParams.get("tab") ?? ""
-  );
+  const [activeTabName, setActiveTabName] = useState("");
 
   useEffect(() => {
-    const activeTabName = searchParams.get("tab");
-    if (activeTabName) setActiveTabName(activeTabName);
-  }, [searchParams]);
+    setActiveTabName(
+      decodeURI(window.location.toString().split("?tab=")[1] ?? "") ?? ""
+    );
+  }, []);
 
   useEffect(() => {
     router.push("/?tab=" + activeTabName);
-  }, [activeTabName]);
+  }, [router, activeTabName]);
 
-  function generateTab(name: string, categoryItems: Item[]): Tab {
-    function updateItem(item: Item) {
-      const itemIndex = items.findIndex((i) => i.id === item.id);
-      setItems(
-        produce(items, (draft) => {
-          draft[itemIndex] = item;
-        })
-      );
-      if (items[itemIndex].category !== item.category)
+  const generateTab = useMemo(() => {
+    return (name: string, categoryItems: Item[]): Tab => {
+      function updateItem(item: Item) {
+        const itemIndex = items.findIndex((i) => i.id === item.id);
+        setItems(
+          produce(items, (draft) => {
+            draft[itemIndex] = item;
+          })
+        );
+        if (items[itemIndex].category !== item.category)
+          setActiveTabName(item.category);
+      }
+      function addItem(item: Item) {
+        setItems(
+          produce(items, (draft) => {
+            draft.push(item);
+          })
+        );
         setActiveTabName(item.category);
-    }
-    function addItem(item: Item) {
-      setItems(
-        produce(items, (draft) => {
-          draft.push(item);
-        })
-      );
-      setActiveTabName(item.category);
-    }
-    function deleteItem(item: Item) {
-      setItems(
-        produce(items, (draft) => {
-          draft.splice(
-            draft.findIndex((draftItem) => draftItem.id === item.id),
-            1
-          );
-        })
-      );
-    }
-
-    return {
-      title: name,
-      value: name,
-      content: (
-        <div className="w-full flex flex-col relative h-full rounded-2xl p-6 text-xl md:text-4xl bg-gradient-to-br from-gray-700 to-gray-900 gap-4 overflow-y-scroll">
-          <p className="font-bold">{name} Products</p>
-          {categoryItems.length > 0 ? (
-            <div className="flex justify-center flex-wrap gap-5">
-              {categoryItems.map((item) => (
-                <ItemCard
-                  key={item.id}
-                  item={item}
-                  updateItem={updateItem}
-                  deleteItem={deleteItem}
-                />
-              ))}
-              {name != "Prioritized" && (
-                <ItemCardForm
-                  partialItem={{ category: name }}
-                  addItem={addItem}
-                />
-              )}
-            </div>
-          ) : (
-            <p>No items in category</p>
-          )}
-        </div>
-      ),
-    };
-  }
-
-  function generateAllTabs(existingTabs: Tab[]) {
-    return produce(existingTabs, (draft) => {
-      const prioritizedItems = items.filter((item) => item.priority);
-      if (draft[0]?.value !== "Prioritized") {
-        if (prioritizedItems.length > 0)
-          draft.splice(0, 0, generateTab("Prioritized", prioritizedItems));
-      } else {
-        draft[0] = generateTab("Prioritized", prioritizedItems);
+      }
+      function deleteItem(item: Item) {
+        setItems(
+          produce(items, (draft) => {
+            draft.splice(
+              draft.findIndex((draftItem) => draftItem.id === item.id),
+              1
+            );
+          })
+        );
       }
 
-      const categories = new Set(items.map((item) => item.category));
-      categories.forEach((category) => {
-        const existingTabIndex = draft.findIndex(
-          (tab) => tab.value === category
-        );
-        const categoryItems = items.filter(
-          (item) => item.category === category
-        );
-        if (existingTabIndex === -1) {
-          draft.push(generateTab(category, categoryItems));
-        } else {
-          draft[existingTabIndex] = generateTab(category, categoryItems);
-        }
-      });
+      return {
+        title: name,
+        value: name,
+        content: (
+          <div className="w-full flex flex-col relative h-full rounded-2xl p-6 text-xl md:text-4xl bg-gradient-to-br from-gray-700 to-gray-900 gap-4 overflow-y-scroll">
+            <p className="font-bold">{name} Products</p>
+            {categoryItems.length > 0 ? (
+              <div className="flex justify-center flex-wrap gap-5">
+                {categoryItems.map((item) => (
+                  <ItemCard
+                    key={item.id}
+                    item={item}
+                    updateItem={updateItem}
+                    deleteItem={deleteItem}
+                  />
+                ))}
+                {name != "Prioritized" && (
+                  <ItemCardForm
+                    partialItem={{ category: name }}
+                    addItem={addItem}
+                  />
+                )}
+              </div>
+            ) : (
+              <p>No items in category</p>
+            )}
+          </div>
+        ),
+      };
+    };
+  }, [items]);
 
-      existingTabs.forEach((existingTab) => {
-        if (
-          existingTab.value !== "Prioritized" &&
-          !categories.has(existingTab.value)
-        ) {
-          draft[draft.findIndex((tab) => tab.value === existingTab.value)] =
-            generateTab(existingTab.value, []);
+  const generateAllTabs = useMemo(() => {
+    return (existingTabs: Tab[]) => {
+      return produce(existingTabs, (draft) => {
+        const prioritizedItems = items.filter((item) => item.priority);
+        if (draft[0]?.value !== "Prioritized") {
+          if (prioritizedItems.length > 0)
+            draft.splice(0, 0, generateTab("Prioritized", prioritizedItems));
+        } else {
+          draft[0] = generateTab("Prioritized", prioritizedItems);
         }
+
+        const categories = new Set(items.map((item) => item.category));
+        categories.forEach((category) => {
+          const existingTabIndex = draft.findIndex(
+            (tab) => tab.value === category
+          );
+          const categoryItems = items.filter(
+            (item) => item.category === category
+          );
+          if (existingTabIndex === -1) {
+            draft.push(generateTab(category, categoryItems));
+          } else {
+            draft[existingTabIndex] = generateTab(category, categoryItems);
+          }
+        });
+
+        existingTabs.forEach((existingTab) => {
+          if (
+            existingTab.value !== "Prioritized" &&
+            !categories.has(existingTab.value)
+          ) {
+            draft[draft.findIndex((tab) => tab.value === existingTab.value)] =
+              generateTab(existingTab.value, []);
+          }
+        });
       });
-    });
-  }
+    };
+  }, [items, generateTab]);
 
   const [tabs, setTabs] = useState(generateAllTabs([]));
   useEffect(() => {
     setTabs((oldTabs) => generateAllTabs(oldTabs));
-  }, [items]);
+  }, [generateAllTabs]);
 
   useEffect(() => {
     const interval = setInterval(async () => {
