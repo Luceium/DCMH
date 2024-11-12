67c52ec6e0c3b4edbe0b716d6c2ef6900c3c4b58
Mau <79354255+Luceium@users.noreply.github.com>
2024-11-12 00:13:05 -0800

    switch analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ import { AuthProvider } from "@propelauth/nextjs/client";
 import Nav from "@/components/nav";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
-import { EditContext, EditContextProvider } from "@/lib/context";
+import { EditContextProvider } from "@/lib/context";
 import { cn } from "@/lib/utils";
-import { Analytics } from "@vercel/analytics/react";
+import { AnalyticsProvider } from "@/lib/analytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,24 +25,25 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <AuthProvider authUrl={process.env.NEXT_PUBLIC_AUTH_URL!}>
-        <body className={cn(inter.className, "bg-[#e0f2ff] dark:bg-black")}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <EditContextProvider>
-              <div className="min-h-[50vh]">
-                <Nav />
-                <div className="pt-4">{children}</div>
-              </div>
-            </EditContextProvider>
-            <Footer />
-            <Toaster />
-          </ThemeProvider>
-          <Analytics />
-        </body>
+        <AnalyticsProvider>
+          <body className={cn(inter.className, "bg-[#e0f2ff] dark:bg-black")}>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <EditContextProvider>
+                <div className="min-h-[50vh]">
+                  <Nav />
+                  <div className="pt-4">{children}</div>
+                </div>
+              </EditContextProvider>
+              <Footer />
+              <Toaster />
+            </ThemeProvider>
+          </body>
+        </AnalyticsProvider>
       </AuthProvider>
     </html>
   );
