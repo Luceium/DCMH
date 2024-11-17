5fb8d55177259d79b9fd5aaa88d69ea51cab08cc
Mauricio Curiel <mauriciom.curiel@gmail.com>
2024-11-16 21:54:15 -0800

    create reusable tabs

--- components/category-tab.tsx
+++ components/category-tab.tsx
@@ -6,15 +6,22 @@ import { deleteCategory, renameCategory } from "@/actions/categories";
 import { X } from "lucide-react";
 import { fetchItems } from "@/actions/fetchItems";
 import { useToast } from "./ui/use-toast";
+import TabField from "./tab-field";
 
+/**
+ * Renders a tab for a category and supports edit mode. Edit mode allows renaming the category and deleting it.
+ * @param alternative - a special prop to indicate a variant for adding tabs (and not representing a category)
+ */
 const CategoryTab = ({
   category,
   invalidateSignal,
   setInvalidateSignal,
+  alternative,
 }: {
   category: Category;
   invalidateSignal: boolean;
   setInvalidateSignal: React.Dispatch<React.SetStateAction<boolean>>;
+  alternative?: React.ReactNode;
 }) => {
   const { edit } = useContext(EditContext);
   const [tabEditMode, setTabEditMode] = React.useState(false);
@@ -31,32 +38,50 @@ const CategoryTab = ({
   }, []);
 
   return (
-    <div>
+    <div className="flex gap-4 items-center bg-background rounded-sm">
       {tabEditMode ? (
-        <input
-          defaultValue={category.name}
-          onKeyDown={(e) => {
-            if (e.key === "Escape") {
-              setTabEditMode(false);
-            }
+        // <input
+        //   defaultValue={category.name}
+        //   onKeyDown={(e) => {
+        //     if (e.key === "Escape") {
+        //       setTabEditMode(false);
+        //     }
 
-            if (e.key === "Enter") {
-              const newName = e.currentTarget.value;
-              if (newName == "") {
-                deleteCategory(category.id);
-              }
-              if (
-                !newName.toLowerCase().includes("prior") &&
-                newName.length > 0
-              ) {
-                renameCategory(category.id, e.currentTarget.value);
-                setTabEditMode(false);
-                setInvalidateSignal(!invalidateSignal);
-              }
+        //     if (e.key === "Enter") {
+        //       const newName = e.currentTarget.value;
+        //       if (newName == "") {
+        //         deleteCategory(category.id);
+        //       }
+        //       if (
+        //         !newName.toLowerCase().includes("prior") &&
+        //         newName.length > 0
+        //       ) {
+        //         renameCategory(category.id, e.currentTarget.value);
+        //         setTabEditMode(false);
+        //         setInvalidateSignal(!invalidateSignal);
+        //       }
+        //     }
+        //   }}
+        // />
+        <TabField
+          defaultValue={category.name}
+          onCancel={() => setTabEditMode(false)}
+          onSubmit={(e) => {
+            const newName = e.currentTarget.value;
+            if (newName == "") {
+              deleteCategory(category.id);
+            }
+            if (
+              !newName.toLowerCase().includes("prior") &&
+              newName.length > 0
+            ) {
+              renameCategory(category.id, e.currentTarget.value);
+              setTabEditMode(false);
+              setInvalidateSignal(!invalidateSignal);
             }
           }}
         />
-      ) : (
+      ) : (alternative ??
         <TabsTrigger
           key={category.name}
           value={category.id}
@@ -70,7 +95,7 @@ const CategoryTab = ({
           {category.name}
         </TabsTrigger>
       )}
-      {edit && (
+      {edit && !tabEditMode && (
         <button
           onClick={() => {
             if (tabEditMode) {
